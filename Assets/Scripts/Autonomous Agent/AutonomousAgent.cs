b0f4131f1740e7cd8ade69790acd7705acca6849
Matthan Cantrell <cantrellmatthan247@gmail.com>
2023-01-12 10:36:05 -0700

    AutoAgent Assignment

--- Assets/Scripts/Autonomous Agent/AutonomousAgent.cs
+++ Assets/Scripts/Autonomous Agent/AutonomousAgent.cs
@@ -6,6 +6,17 @@ using UnityEngine;
 
 public class AutonomousAgent : Agent
 {
+    public Perception flockPerception;
+
+    public float wanderDistance = 1;
+    public float wanderRadius = 3;
+    public float wanderDisplacement = 5;
+
+    [Range(0, 3)] public float SeekWeight = 0;
+    [Range(0, 3)] public float FleeWeight = 0;
+
+    public float wanderAngle { get; set; } = 0;
+
     // Update is called once per frame
     void Update()
     {
@@ -16,8 +27,24 @@ public class AutonomousAgent : Agent
         }
         if (gameObjects.Length > 0)
         {
-            movement.ApplyForce(Steering.Flee(this, gameObjects[0]));
+            movement.ApplyForce(Steering.Flee(this, gameObjects[0]) * FleeWeight);
+            movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * SeekWeight);
         }
+
+        gameObjects = flockPerception.GetGameObjects();
+
+        if (gameObjects.Length > 0)
+        {
+            //
+        }
+
+        if (movement.acceleration.sqrMagnitude <= movement.maxForce * 0.1f)
+        {
+            movement.ApplyForce(Steering.Wander(this));
+        }
+
         transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
     }
+
+
 }
