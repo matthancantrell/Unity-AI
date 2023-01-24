57e3f6cee8bfedb761358fe76aaeac165cdfe5aa
Matthan Cantrell <cantrellmatthan247@gmail.com>
2023-01-23 20:32:29 -0700

    ur mother

--- Assets/Scripts/AI Course/Utilities/AgentSpawner.cs
+++ Assets/Scripts/AI Course/Utilities/AgentSpawner.cs
@@ -4,27 +4,25 @@ using UnityEngine;
 
 public class AgentSpawner : MonoBehaviour
 {
-    public Agent agent01;
-    public Agent agent02;
+    public Agent[] agents;
     public LayerMask layerMask;
+    int index = 0;
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.Tab)) index = (++index % agents.Length);
+
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+/*            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hitInfo, 100, layerMask))
             {
                 Instantiate(agent01, hitInfo.point, Quaternion.identity);
-            }
+            }*/
         }
-        if (Input.GetMouseButtonDown(1))
+/*        if (Physics.Raycast(ray out RaycastHit hitInfo, 100, layerMask))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hitInfo, 100, layerMask))
-            {
-                Instantiate(agent02, hitInfo.point, Quaternion.identity);
-            }
-        }
+            Instantiate(agents[index], hitInfo.point, Quaternion.AngleAxis(Random.Range(0, 360)), );
+        }*/
     }
 }
