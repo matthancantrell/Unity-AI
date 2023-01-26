41eecae60babcaa7f76a5f65f9a27e2b5210efa0
Matthan Cantrell <cantrellmatthan247@gmail.com>
2023-01-26 11:54:59 -0700

    Game Scripts Obtained. Will Update after videos tonight

---
+++ Assets/A/Intro To Game Engines/Roller Game/Scripts/Coin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CollisionEvent))]
+public class Coin : Interactable
+{
+	void Start()
+	{
+		GetComponent<CollisionEvent>().onEnter += OnInteract;
+	}
+
+	public override void OnInteract(GameObject go)
+	{
+		if (go.TryGetComponent<RollerPlayer>(out RollerPlayer player))
+		{
+			player.AddPoints(100);
+		}
+
+		if (interactFX != null) Instantiate(interactFX, transform.position, Quaternion.identity);
+		if (destroyOnInteract) Destroy(gameObject);
+	}
+}
---
+++ Assets/A/Intro To Game Engines/Roller Game/Scripts/RollerPlayer.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RollerPlayer : MonoBehaviour
+{
+	[SerializeField] private Transform view;
+	[SerializeField] private float maxForce = 5;
+	[SerializeField] private float groundRayLength = 1;
+	[SerializeField] private LayerMask groundLayer;
+
+	private int score = 0;
+	private Vector3 force;
+	private Rigidbody rb;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		view = Camera.main.transform;
+		Camera.main.GetComponent<RollerCamera>().SetTarget(transform);
+
+		//subscribing to events
+		GetComponent<Health>().onDamage += OnDamage;
+		GetComponent<Health>().onDeath += OnDeath;
+		RollerGameManager.Instance.SetHealth((int)GetComponent<Health>().health);
+	}
+
+	void Update()
+	{
+		Vector3 direction = Vector3.zero;
+
+		direction.x = Input.GetAxis("Horizontal");
+		direction.z = Input.GetAxis("Vertical");
+
+		Quaternion viewSpace = Quaternion.AngleAxis(view.rotation.eulerAngles.y, direction);
+		force = viewSpace * (direction * maxForce);
+
+		Ray ray = new Ray(transform.position, Vector3.down);
+		bool onGround = Physics.Raycast(ray, groundRayLength, groundLayer);
+		Debug.DrawRay(transform.position, ray.direction * groundRayLength);
+
+		if (onGround && Input.GetButtonDown("Jump"))
+		{
+			rb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		rb.AddForce(force);
+	}
+
+	public void AddPoints(int points)
+	{
+		score += points;
+		RollerGameManager.Instance.SetScore(score);
+	}
+
+	public void OnDamage()
+	{
+		Debug.Log("ouch!");
+		RollerGameManager.Instance.SetHealth((int)GetComponent<Health>().health);
+	}
+
+	public void OnDeath()
+	{
+		RollerGameManager.Instance.SetGameOver();
+	}
+}
